27e61a36a24512dad6525f5df820239eaa2c7d45
Shiqi Wang <shiwa@microsoft.com>
2017-10-20 11:43:11 -0700

    Pass unity activity context to plugin, print it on screen

---
+++ UnityProject/UnityImageLoader/Assets/PluginWrapper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PluginWrapper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        TextMesh textMesh = GetComponent<TextMesh>();
+
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); 
+        AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+
+        var plugin = new AndroidJavaClass("microsoft.prototype.braodcastsupport.ImageLoader");
+        //textMesh.text = plugin.CallStatic<string>("loadTextureIdStr", "/storage/emulated/0/myscreen_7.png");
+        textMesh.text = plugin.CallStatic<string>("setContext", jo);
+	}
+}
