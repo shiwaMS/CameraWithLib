cf4150a2d3d4b3b54d0a6f297c912c98117daa4a
Shiqi Wang <shiwa@microsoft.com>
2017-10-20 16:19:38 -0700

    CreateExternalTexture not quite working, TBC

--- UnityProject/UnityImageLoader/Assets/PluginWrapper.cs
+++ UnityProject/UnityImageLoader/Assets/PluginWrapper.cs
@@ -10,58 +10,41 @@ public class PluginWrapper : MonoBehaviour
     public RawImage display;
     public AspectRatioFitter fit;
 
-
+    private Texture2D texture2D;
+    private Int32 texturePtr;
     // Use this for initialization
     void Start()
     {
-        //TextMesh textMesh = GetComponent<TextMesh>();
-
-        //AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); 
-        //AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
-
-        //var plugin = new AndroidJavaClass("microsoft.prototype.broadcastsupport.ImageLoader");
-        //textMesh.text = plugin.CallStatic<string>("loadTextureIdStr", jo, "/storage/emulated/0/myscreen_7.png");
-        //textMesh.text = plugin.CallStatic<string>("setContext", jo);
-
-        Texture2D texture2D = new Texture2D(540, 960, TextureFormat.ARGB32, false);
+        this.texture2D = new Texture2D(2, 2); ;
 
         var plugin = new AndroidJavaClass("microsoft.prototype.broadcastsupport.ImageLoader");
-        Int32 texturePtr = plugin.CallStatic<Int32>("loadTextureId", "/storage/emulated/0/myscreen_7.png");
-        Debug.Log("texture pointer = " + texturePtr);
+        this.texturePtr = plugin.CallStatic<Int32>("loadTextureId", "/storage/emulated/0/myscreen_7.png");
+        Debug.Log("texture pointer = " + this.texturePtr);
+        if (texturePtr > 0)
+        {
+            Texture2D nativeTexture = Texture2D.CreateExternalTexture(540, 960, TextureFormat.ARGB32, false, true, new IntPtr(this.texturePtr));
+            if (nativeTexture == null)
+            {
+                Debug.Log("nativeTexture object is null ");
+                return;
+            }
+            else
+            {
+                Debug.Log("nativeTexture pixel length: " +
+                        nativeTexture.GetPixels().Length);
+                Debug.Log("nativeTexture width x height: " + nativeTexture.width + " x " + nativeTexture.height);
+                this.texture2D.UpdateExternalTexture(nativeTexture.GetNativeTexturePtr());
+                Debug.Log("texture2D width x height: " + this.texture2D.width + " x " + this.texture2D.height); // somehow it is 2x2
 
-        //if (texturePtr > 0)
-        //{
-        //    Texture2D nativeTexture = Texture2D.CreateExternalTexture(540, 960, TextureFormat.ARGB32, false, false, (IntPtr)texturePtr);
-        //    if (nativeTexture == null)
-        //    {
-        //        Debug.Log("nativeTexture object is null ");
-        //        return;
-        //    }
-        //    else
-        //    {
-        //        Debug.Log("nativeTexture pixel length: " +
-        //                nativeTexture.GetPixels().Length);
-        //        texture2D.UpdateExternalTexture(nativeTexture.GetNativeTexturePtr());
-        //        display.texture = texture2D;
-        //    }
+                display.texture = this.texture2D;
 
-        //    float ratio = 540.0f / 960.0f;
-        //    fit.aspectRatio = ratio;
-        //}
-
-        byte[] fileData = File.ReadAllBytes("/storage/emulated/0/myscreen_7.png");
-        Texture2D tex = new Texture2D(2, 2);
-        tex.LoadImage(fileData);
-
-        float ratio = (float)tex.width / (float)tex.height;
-        fit.aspectRatio = ratio;
-        Debug.Log("texture ratio = " + ratio);
-
-        display.texture = tex;
+                float ratio = 540.0f / 960.0f;
+                fit.aspectRatio = ratio;
+                Debug.Log("texture ratio = " + ratio);
+            }
+        }
     }
-
 
     void Update()
     { }
-
 }
