6b069943be47979ecc769ed2aae4e1eb010e45ca
Shiqi Wang <shiwa@microsoft.com>
2017-10-19 18:48:04 -0700

    Verified that load texture ID actually loads

--- app/src/main/java/microsoft/prototype/camerawithlib/Activities/LoadImageActivity.java
+++ app/src/main/java/microsoft/prototype/camerawithlib/Activities/LoadImageActivity.java
@@ -5,16 +5,14 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.ActionMode;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import microsoft.prototype.braodcastsupport.LoadImage;
+import microsoft.prototype.braodcastsupport.ImageLoader;
 import microsoft.prototype.camerawithlib.R;
 
 public class LoadImageActivity extends AppCompatActivity {
@@ -22,6 +20,7 @@ public class LoadImageActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private Button loadButton;
+    private TextView imageSizeTextView;
     private TextView textView;
     private EditText editText;
     private String filePath;
@@ -33,6 +32,8 @@ public class LoadImageActivity extends AppCompatActivity {
 
         this.imageView = (ImageView) findViewById(R.id.image_view);
 
+        this.imageSizeTextView = (TextView) findViewById(R.id.image_size_text);
+
         this.textView = (TextView) findViewById(R.id.image_folder_text);
         final String folderPath = Environment.getExternalStorageDirectory().toString();
         this.textView.setText(folderPath);
@@ -41,7 +42,8 @@ public class LoadImageActivity extends AppCompatActivity {
         this.editText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!TextUtils.isEmpty(folderPath) && TextUtils.isEmpty(editText.getText())){}
+                if (!TextUtils.isEmpty(folderPath) && TextUtils.isEmpty(editText.getText())) {
+                }
                 editText.setText(folderPath);
                 editText.setSelection(editText.getText().length());
             }
@@ -54,9 +56,11 @@ public class LoadImageActivity extends AppCompatActivity {
                 if (editText != null) {
                     filePath = editText.getText().toString();
                     if (!TextUtils.isEmpty(filePath)) {
-                        Bitmap loadedImg = LoadImage.loadBitmap(filePath);
+                        Bitmap loadedImg = ImageLoader.loadBitmap(filePath);
                         if (loadedImg != null) {
-                            imageView.setImageBitmap(LoadImage.loadBitmap(filePath));
+                            imageView.setImageBitmap(ImageLoader.loadBitmap(filePath));
+                            imageSizeTextView.setText(loadedImg.getWidth() + " x " + loadedImg.getHeight());
+                            Log.i(TAG, "Image loaded, size: " + loadedImg.getWidth() + " x " + loadedImg.getHeight());
                         }
                     }
                 }
--- braodcastsupport/src/main/java/microsoft/prototype/braodcastsupport/ImageLoader.java
+++ braodcastsupport/src/main/java/microsoft/prototype/braodcastsupport/ImageLoader.java
@@ -3,13 +3,13 @@ package microsoft.prototype.braodcastsupport;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.opengl.GLES20;
+import android.opengl.GLUtils;
 import android.util.Log;
 
-import java.io.InputStream;
 import java.nio.ByteBuffer;
 
-public class LoadImage {
-    private static final String TAG = LoadImage.class.getSimpleName();
+public class ImageLoader {
+    private static final String TAG = ImageLoader.class.getSimpleName();
 
     public static Bitmap loadBitmap(String fileName) {
         Log.i(TAG, "loadBitmap path: " + fileName);
@@ -21,7 +21,8 @@ public class LoadImage {
     public static int loadTextureId(String fileName) {
         Log.d(TAG, "loadTextureId path: " + fileName);
         Bitmap bitmap = BitmapFactory.decodeFile(fileName);
-        Log.d(TAG, "Bitmap is: " + bitmap);
+//        Log.d(TAG, "Bitmap is: " + bitmap);
+        Log.i(TAG, "Image loaded, size: " + bitmap.getWidth() + " x " + bitmap.getHeight());
         ByteBuffer buffer = ByteBuffer.allocate(bitmap.getByteCount());
         bitmap.copyPixelsToBuffer(buffer);
         int textures[] = new int[1];
@@ -29,7 +30,7 @@ public class LoadImage {
         int textureId = textures[0];
         GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId);
-        GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGBA, 1920, 1080, 0, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, buffer);
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D,0, bitmap,0);
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S,
