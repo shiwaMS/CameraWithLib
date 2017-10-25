82c7bde4cec1344fc8b103596921c32125cd1c48
Shiqi Wang <shiwa@microsoft.com>
2017-10-24 17:18:42 -0700

    Get textureId from plugin try to display it in Unity

--- UnityProject/UnityImageLoader/Assets/PluginWrapper.cs
+++ UnityProject/UnityImageLoader/Assets/PluginWrapper.cs
@@ -12,17 +12,19 @@ public class PluginWrapper : MonoBehaviour
 
     private Texture2D texture2D;
     private Int32 texturePtr;
+
     // Use this for initialization
     void Start()
     {
-        this.texture2D = new Texture2D(2, 2); ;
-
-        var plugin = new AndroidJavaClass("microsoft.prototype.broadcastsupport.ImageLoader");
-        this.texturePtr = plugin.CallStatic<Int32>("loadTextureId", "/storage/emulated/0/myscreen_7.png");
+        AndroidJavaClass plugin = new AndroidJavaClass("microsoft.prototype.broadcastsupport.ImageLoader");
+        this.texturePtr = plugin.CallStatic<Int32>("loadTextureId");
         Debug.Log("texture pointer = " + this.texturePtr);
+
+        this.texture2D = new Texture2D(2, 2);
+
         if (texturePtr > 0)
         {
-            Texture2D nativeTexture = Texture2D.CreateExternalTexture(540, 960, TextureFormat.ARGB32, false, true, new IntPtr(this.texturePtr));
+            Texture2D nativeTexture = Texture2D.CreateExternalTexture(144, 144, TextureFormat.ARGB32, false, true, new IntPtr(this.texturePtr));
             if (nativeTexture == null)
             {
                 Debug.Log("nativeTexture object is null ");
@@ -30,15 +32,15 @@ public class PluginWrapper : MonoBehaviour
             }
             else
             {
-                Debug.Log("nativeTexture pixel length: " +
-                        nativeTexture.GetPixels().Length);
+                Debug.Log("nativeTexture pixel length: " + nativeTexture.GetPixels().Length);
                 Debug.Log("nativeTexture width x height: " + nativeTexture.width + " x " + nativeTexture.height);
+
                 this.texture2D.UpdateExternalTexture(nativeTexture.GetNativeTexturePtr());
-                Debug.Log("texture2D width x height: " + this.texture2D.width + " x " + this.texture2D.height); // somehow it is 2x2
+                Debug.Log("texture2D width x height: " + this.texture2D.width + " x " + this.texture2D.height);
 
                 display.texture = this.texture2D;
 
-                float ratio = 540.0f / 960.0f;
+                float ratio = 144.0f / 144.0f;
                 fit.aspectRatio = ratio;
                 Debug.Log("texture ratio = " + ratio);
             }
